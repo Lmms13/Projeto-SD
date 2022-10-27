95cfc2c8c40030355fa2a1e61e401a0a6f040617
Luis <fc56341@alunos.fc.ul.pt>
2022-10-27 19:57:18 +0100

    tree_client.c feito

--- src/tree_client.c
+++ src/tree_client.c
@@ -1,84 +1,148 @@
-
 #include <stdio.h>
 #include <string.h>
 #include <client_stub.h>
 
-
-#define BUFFERSIZE 10
-int main(int argc, char const *argv[]){
+int main(int argc, char *argv[]){
 
     if(argc != 2){
-        printf("Incluir o endereco IP ou nome do servidor da arvore e o numero do porto TCP aoqual o servidor esta a espera de ligacoes! \nExemplo de utilizacao: ./bin/tree-client<server>:<port>\n");
-                return -1;
-    }
-    // Retirar valores de ip e numero de port
-    const char[] *server_ip = argv[0];
-    const char[] *port_str = argv[1];
-    struct rtree_t *tree;
-    tree = rtree_connect(port_str);
-    if(tree == NULL){
-        printf("Error\n\t Association between client and server led to an errorAssocia to server\n");
+        printf("Incluir o IP ou nome do servidor e o numero do porto TCP do servidor!\nExemplo de utilizacao: ./bin/tree-client<server>:<port>\n");
         return -1;
     }
-    while(true){
-        char buffer[BUFFERSIZE];
-        printf("Enter a command: \n");
-        fgets(buffer, BUFFERSIZE , stdin);
-        const char space = " ";
-        char *token;
-   
-        /* get the first token */
-        token = strtok(buffer, space);
-        /* walk through other tokens */
-        while( token != NULL ) {
-            printf( " %s\n", token );
-        }   
-        token = strtok(NULL, s);
-                
-        if(strcomp(token,"size") == 0){
-            int size = rtree_size(tree);
-            printf("Number of elements in the tree : %d \n", size);
-        }else if(strcomp(token,"height")== 0){
-            int height = rtree_height(tree);
-            printf("The height of the tree is %d \n", height)
-        }else if(strcomp(token,"del")== 0){
-            int del = rtree_del(tree, );
-            if(del == 0){
-                printf("OK");
-            }else if(del == -1){
-                printf("ERROR \n\t Key not found or other problems\n")
-            }
-        }else if(strcomp(token,"get")== 0){
-            struct data_t data = rtree_get(tree, );
-            if(&data == NULL){
-                printf("ERROR \n\t Error at getting element\n");
-            }
-        }else if(strcomp(token,"put")== 0){
-            int put = rtree_put(tree);
-            if(put == 0){
-                printf("OK");
-            }else if(put == -1){
-                printf("ERROR\n\t Problems occurred\n");
-            }
-        }else if(strcomp(token,"getKeys")== 0){
-            char* keys = rtree_get_keys(tree);
-            printf("%s", keys);
-        }else if(strcomp(token,"getvalues")== 0){
-            rtree_get_values(tree);
-        }else if(strcomp(token,"quit")== 0){
-            int disconnect = rtree_disconnect(tree);
-            if(disconnect == 0){
-                printf("Client closing\n");
-            }else if(disconnect == -1){
-                printf("ERROR \n\t Error at disconnect\n");
-            }      
-            break;
-        }else {
-            printf("Command not recognized \n Try again\n");
-        }
-          
+
+    struct rtree_t *tree;
+    tree = rtree_connect(argv[1]);
+    if(tree == NULL){
+        printf("Ocorreu um erro a associar o cliente ao servidor!\n");
+        return -1;
     }
-    
-    return 0;
-    
+
+    while(1){
+        char input[100];
+        printf("\nInsira um dos seguintes comandos:\n");
+        printf("put <key> <data>\n");
+        printf("get <key>\n");
+        printf("del <key>\n");
+        printf("size\n");
+        printf("height\n");
+        printf("getkeys\n");
+        printf("getvalues\n");
+        printf("quit\n\n");
+
+        fgets(input, 100, stdin);
+        char *token = strtok(input, " \n");
+
+        if(strcmp(token, "put") == 0){
+            char *key = strtok(NULL, " ");
+            char *input_data = strtok(NULL, "\n");
+            if(key == NULL || input_data == NULL){
+                print("O comando put tem de incluir uma chave e dados!\n");
+                print("Exemplo: put <key> <data>\n");
+                continue;
+            }
+
+            int size = strlen(input_data)+1;
+
+            struct data_t *data = data_create2(size, input_data);
+            struct entry_t *entry = entry_create(key, data);
+
+            rtree_put(tree, entry);
+            printf("-----------------------\n");
+        }
+
+        else if(strcmp(token, "get") == 0){
+            char *key = strtok(NULL, " ");
+            if(key == NULL){
+                print("O comando get tem de incluir uma chave!\n");
+                print("Exemplo: get <key>\n");
+                continue;
+            }
+
+            struct data_t *data = rtree_get(tree, key);
+            //fazer alguma coisa com a data
+            data_destroy(data);
+            printf("-----------------------\n");
+        }
+
+        else if(strcmp(token, "del") == 0){
+            char *key = strtok(NULL, " ");
+            if(key == NULL){
+                print("O comando del tem de incluir uma chave!\n");
+                print("Exemplo: del <key>\n");
+                continue;
+            }
+
+            rtree_del(tree, key);
+            printf("-----------------------\n");
+        }
+
+        else if(strcmp(token, "size") == 0){
+            rtree_size(tree);
+            printf("-----------------------\n");
+        }
+
+        else if(strcmp(token, "height") == 0){
+            rtree_height(tree);
+            printf("-----------------------\n");
+        }
+
+        else if(strcmp(token, "getkeys") == 0){
+            char **keys = rtree_get_keys(tree);
+            if(keys == NULL){
+                printf("Ocorreu um erro a obter as chaves!\n");
+                continue;
+            }
+            else{
+                for(int i = 0;keys[i] != NULL; i++){
+                    if(keys[i] != NULL){
+                        printf("%s\n", keys[i]);
+                    }
+                }
+
+                for(int i = 0;keys[i] != NULL; i++){
+                    if(keys[i] != NULL){
+                        free(keys[i]);
+                    }
+                }
+                free(keys);
+            }
+            printf("-----------------------\n");
+        }
+
+        else if(strcmp(token, "getvalues") == 0){
+            struct data_t **values = rtree_get_values(tree);
+            if(values == NULL){
+                printf("Ocorreu um erro a obter os valores!\n");
+                continue;
+            }
+            else{
+                for(int i = 0;values[i] != NULL; i++){
+                    if(values[i] != NULL){
+                        printf("%s\n", (char *)values[i]->data);
+                    }
+                }
+
+                for(int i = 0;values[i] != NULL; i++){
+                    if(values[i] != NULL){
+                        data_destroy(values[i]);
+                    }
+                }
+                free(values);
+            }
+            printf("-----------------------\n");
+        }
+
+        else if(strcmp(token, "quit") == 0){
+            printf("A terminar a execucao...\n");
+            if(rtree_disconnect(tree) == -1){
+                printf("Ocorreu um erro a desconectar!\n");
+            }
+            printf("-----------------------\n");
+            break;
+        }
+
+        else{
+            printf("Comando nao reconhecido!\n");
+        }      
+    }  
+    return 0;  
 }
--- src/tree_server.c
+++ src/tree_server.c
@@ -4,7 +4,7 @@
 int main(int argc, char const *argv[]){
     if(argc != 2){
         printf("Incluir o numero do porto TCP ao qual o servidor se deve associar!\nExemplo de utilizacao: ./bin/tree-server <port>\n");
-                return -1;
+        return -1;
     }
 
     int socket = network_server_init(atoi(argv[1]));
@@ -28,7 +28,7 @@ int main(int argc, char const *argv[]){
         printf("Ocorreu um erro\n");
         return -1;
     }
-    
+
     tree_skel_destroy();
     return 0;
 }
